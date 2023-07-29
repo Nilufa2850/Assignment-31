0c31f96c26e07b6be36216ed4de50804fb537048
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-07-29 10:50:58 +0530

    Create Qn-06.cpp

---
+++ Qn-06.cpp
@@ -0,0 +1,67 @@
+/*6. Write a C++ program to demonstrate how a common friend function can
+be used to exchange the private values of two classes. (Use call by
+reference method).*/
+
+#include<iostream>
+using namespace std ;
+
+class Second ;      // becoz we use of Second class inside First class
+class First
+{
+    private : 
+        int a , b ;
+    public : 
+        void setData(int A , int B)
+        {
+            a = A ;
+            b = B ;
+        }
+        void showData()
+        {
+            cout<<"a = "<<a<<" b = "<<b<<endl ;
+        }
+        friend void exchange (First & , Second & )  ;
+} ;
+class Second
+{
+    private : 
+        int x , y ;
+    public :
+        void setData_sec(int X, int Y)
+        {
+            x = X ;
+            y = Y ;
+        }
+        void showData_sec()
+        {
+            cout<<"x = "<<x<<" y = "<<y<<endl ;
+        }
+    
+        friend void exchange (First & , Second & )  ;
+} ;
+
+void exchange (First &m, Second &n ) 
+{
+    First temp ;
+    temp.a = m.a ;
+    m.a = n.x ;
+    n.x = temp.a ;
+
+    cout<<"after swapping : "<<endl ;
+    cout<<"a = "<<m.a<<" x = "<<n.x<<endl ;
+}
+
+int main ()
+{
+    First f ;
+    f.setData(5,6) ;
+    f.showData();
+
+    Second s ;
+    s.setData_sec(9,10) ;
+    s.showData_sec() ;
+
+    exchange(f , s) ;
+
+    
+}
